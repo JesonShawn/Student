7f45a3282f84d7376e9b26dee30bafd10e4785ac
Jeson <Jeson@LAPTOP-83ALGK97>
2020-11-09 15:24:49 +0530

    Generic Response bean/entity structure

---
+++ demo/src/main/java/com/example/response/Success.java
@@ -0,0 +1,25 @@
+package com.example.response;
+
+public class Success {
+
+	String ref;
+	String message;
+	public String getRef() {
+		return ref;
+	}
+	public void setRef(String ref) {
+		this.ref = ref;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+
+	
+	
+	
+	
+}
